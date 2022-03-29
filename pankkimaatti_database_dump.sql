2a6b513635f51a45fb9b62e0155f87405a5dd921
Santeri Uusitalo <sanesante@gmail.com>
2022-03-29 12:46:10 +0300

    tietokanta v1

---
+++ pankkimaatti_database_dump.sql
@@ -0,0 +1,161 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: testimaatti
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `asiakas`
+--
+
+DROP TABLE IF EXISTS `asiakas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asiakas` (
+  `id_asiakas` int NOT NULL AUTO_INCREMENT,
+  `nimi` varchar(45) DEFAULT NULL,
+  `lahiosoite` varchar(45) DEFAULT NULL,
+  `puhelinnumero` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_asiakas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asiakas`
+--
+
+LOCK TABLES `asiakas` WRITE;
+/*!40000 ALTER TABLE `asiakas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asiakas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kortti`
+--
+
+DROP TABLE IF EXISTS `kortti`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kortti` (
+  `id_kortti` int NOT NULL AUTO_INCREMENT,
+  `korttinumero` int DEFAULT NULL,
+  `pin` varchar(255) DEFAULT NULL,
+  `id_asiakas` int DEFAULT NULL,
+  `id_tili` int DEFAULT NULL,
+  PRIMARY KEY (`id_kortti`),
+  KEY `id_tili` (`id_tili`),
+  KEY `id_asiakas` (`id_asiakas`),
+  CONSTRAINT `kortti_ibfk_1` FOREIGN KEY (`id_tili`) REFERENCES `tili` (`id_tili`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `kortti_ibfk_2` FOREIGN KEY (`id_asiakas`) REFERENCES `asiakas` (`id_asiakas`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kortti`
+--
+
+LOCK TABLES `kortti` WRITE;
+/*!40000 ALTER TABLE `kortti` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kortti` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tili`
+--
+
+DROP TABLE IF EXISTS `tili`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tili` (
+  `id_tili` int NOT NULL AUTO_INCREMENT,
+  `tilinumero` varchar(45) DEFAULT NULL,
+  `saldo` float DEFAULT NULL,
+  PRIMARY KEY (`id_tili`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tili`
+--
+
+LOCK TABLES `tili` WRITE;
+/*!40000 ALTER TABLE `tili` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tili` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tili_asiakas`
+--
+
+DROP TABLE IF EXISTS `tili_asiakas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tili_asiakas` (
+  `id_tili` int DEFAULT NULL,
+  `id_asiakas` int DEFAULT NULL,
+  KEY `id_tili` (`id_tili`),
+  KEY `id_asiakas` (`id_asiakas`),
+  CONSTRAINT `tili_asiakas_ibfk_1` FOREIGN KEY (`id_tili`) REFERENCES `tili` (`id_tili`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `tili_asiakas_ibfk_2` FOREIGN KEY (`id_asiakas`) REFERENCES `asiakas` (`id_asiakas`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tili_asiakas`
+--
+
+LOCK TABLES `tili_asiakas` WRITE;
+/*!40000 ALTER TABLE `tili_asiakas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tili_asiakas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tilitapahtumat`
+--
+
+DROP TABLE IF EXISTS `tilitapahtumat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tilitapahtumat` (
+  `id_tilitapahtumat` int NOT NULL AUTO_INCREMENT,
+  `paivays` datetime DEFAULT NULL,
+  `tapahtuma` varchar(45) DEFAULT NULL,
+  `summa` float DEFAULT NULL,
+  `id_tili` int DEFAULT NULL,
+  `id_kortti` int DEFAULT NULL,
+  PRIMARY KEY (`id_tilitapahtumat`),
+  KEY `id_tili` (`id_tili`),
+  CONSTRAINT `tilitapahtumat_ibfk_1` FOREIGN KEY (`id_tili`) REFERENCES `tili` (`id_tili`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tilitapahtumat`
+--
+
+LOCK TABLES `tilitapahtumat` WRITE;
+/*!40000 ALTER TABLE `tilitapahtumat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tilitapahtumat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-28 16:55:54
