0c956a04d4bbeada9e614d36b74e0cf151ad2da3
biist <72079769+JanneOhtonen@users.noreply.github.com>
2022-04-01 11:14:50 +0300

    tilitapahtumat päivitetty

---
+++ api/models/tilitapahtumat_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const tilitapahtumat = {
+  getById: function(id, callback) {
+    return db.query('select * from tilitapahtumat where idtilitapahtumat=?', [id], callback);
+  },
+  getAll: function(callback) {
+    return db.query('select * from tilitapahtumat', callback);
+  },
+  add: function(tilitapahtumat, callback) {
+    return db.query(
+      'insert into tilitapahtumat (idtilitapahtumat,paivays,tapahtuma,summa,idtili) values(?,?,?,?,?,?)',
+      [tilitapahtumat.idtilitapahtumat, tilitapahtumat.paivays, tilitapahtumat.tapahtuma, tilitapahtumat.summa, tilitapahtumat.idtili, tilitapahtumat.idkortti],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from tilitapahtumat where idtilitapahtumat=?', [id], callback);
+  },
+  update: function(id, tilitapahtumat, callback) {
+    return db.query(
+      'update tilitapahtumat set idtilitapahtumat=?,paivays=?, tapahtuma=?, summa=?, idtili=?, idkortti=? where idtilitapahtumat=?',
+      [tilitapahtumat.idtilitapahtumat, tilitapahtumat.paivays, tilitapahtumat.tapahtuma, tilitapahtumat.summa, tilitapahtumat.idtili, tilitapahtumat.idkortti],
+      callback
+    );
+  }
+};
+module.exports = tilitapahtumat;
